cd2ff344ef45ff32bceb3eee8eba1ea88e5ca30b
LeiteJoia <tonbellusci@hotmail.com>
2025-10-02 20:01:57 -0300

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-
-function toggleMenu() {
-  const menu = document.getElementById('site-menu');
-  const expanded = menu.style.display === 'flex';
-  menu.style.display = expanded ? 'none' : 'flex';
-}
